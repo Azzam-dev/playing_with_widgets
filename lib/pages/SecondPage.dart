4d8b99f82a3ce8ad3f324e21e604301a03e64d6c
Azzam R Alrashed <azzam.rar@gmail.com>
2023-03-13 19:17:33 +0300

    added the Hero wiget to the second page

--- lib/pages/SecondPage.dart
+++ lib/pages/SecondPage.dart
@@ -7,11 +7,58 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange[300],
-      child: Center(
-        child: Text('page 2'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('page 2'),
+        backgroundColor: Colors.orange,
       ),
+      body: Container(
+        color: Colors.orange[300],
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DashPage(),
+                  ));
+            },
+            child: Hero(
+              tag: "flutter_dash",
+              child: Icon(
+                Icons.flutter_dash,
+                size: 100,
+              ),
+            ),
+            style: TextButton.styleFrom(foregroundColor: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DashPage extends StatelessWidget {
+  const DashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Hero(
+              tag: "flutter_dash",
+              child: Icon(
+                Icons.flutter_dash,
+                size: 200,
+              ),
+            ),
+            Text("this is a photo of a beard")
+          ],
+        ),
+      ),
+      backgroundColor: Colors.orange[100],
     );
   }
 }
