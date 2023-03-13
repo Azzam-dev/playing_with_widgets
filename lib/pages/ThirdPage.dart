c871d8bee5f526f874b81ea47d4a58a8b6f2f067
Azzam R Alrashed <azzam.rar@gmail.com>
2023-03-13 19:39:49 +0300

    worked on the thirdPage

--- lib/pages/ThirdPage.dart
+++ lib/pages/ThirdPage.dart
@@ -1,16 +1,72 @@
 import 'package:flutter/material.dart';
 
-class ThirdPage extends StatelessWidget {
-  const ThirdPage({
+class ThirdPage extends StatefulWidget {
+  ThirdPage({
     super.key,
   });
+
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  double xAlignment = 0;
+  double yAlignment = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue[300],
       child: Center(
-        child: Text('page 3'),
+        child: Stack(
+          children: [
+            AnimatedAlign(
+              duration: Duration(seconds: 1),
+              alignment: Alignment(xAlignment, yAlignment),
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.white,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              yAlignment -= 0.1;
+                            });
+                          },
+                          child: Text("up")),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              yAlignment += 0.1;
+                            });
+                          },
+                          child: Text("down")),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
